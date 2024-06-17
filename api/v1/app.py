cd90a8c5c07986a2bd59b17528e7686792f8d272
chifundo biziweck <chifundobiziweck@gmail.com>
2024-06-17 23:30:50 +0000

    Status of you API

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,10 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 
-app.registerBlueprint(app_views)
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_current_session(excp):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-from flask import jsonfy
+""" Index containing flask routes """
+from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route("/status")
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """Shows the status of the api"""
-    return jsonfy({"status": "OK"})
+    return jsonify({"status": "OK"})
